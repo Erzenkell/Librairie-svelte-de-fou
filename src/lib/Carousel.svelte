d99eedb6eb119e41a9016b821b235aa3f03f047b
Spudwebb96 <jonas.janko17@gmail.com>
2022-09-07 19:04:29 +0200

    Carousel
    Required :
    https://tailwindcss.com/docs/guides/sveltekit - for Tailwind
    
    https://svelte.dev/blog/svelte-and-typescript - for Typescript support
    
    https://redpixelthemes.com/blog/tailwindcss-hide-scrollbar/ - for Hide scrollbar class

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    type ImageType = {
+        title: string
+        src: string
+    }
+    export let images: ImageType[]
+</script>
+
+<ul id='default-carousel' class="flex scrollbar-hide overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]" data-carousel="static">
+    
+    {#each images as {title,src} }
+    <li class="self-center shrink-0 snap-center" data-carousel-item>
+        <img class="" {title} {src} alt={title} />
+    </li>     
+    {/each}
+
+</ul>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+import Carousel from "../lib/Carousel.svelte";
+
+    export const prerender = true
+    
+    let images = [
+        {title: 'a', src: '../bg1.jpg'},
+        {title: 'b', src: '../bg2.jpg'},
+        {title: 'c', src: '../bg3.jpg'},
+        {title: 'd', src: '../bg4.jpg'},
+        {title: 'e', src: '../bg5.jpg'},
+    ]
+
+    import Tabs from '../lib/Tab.svelte'
+    import Timer from '../lib/Timer.svelte'
+    import List from '../lib/List.svelte'
+    import Button from '../lib/Button.svelte'
+    import ContactCard from '../lib/ContactCard.svelte'
+    import Modal from '../lib/Modal.svelte'
+    import Navbar from '../lib/Navbar.svelte'
+  
+    let schools = ['HETIC', 'EEMI', 'Epitech', 'Etna']
+    let toStrong = 'HETIC'
+  
+    let tabItems = ['Home', 'Not Home', '???']
+    let activeItem = 'Home'
+  
+    const triggerTabChange = (event) => {
+      activeItem = event.detail
+    }
+  
+    let showModal = false
+    let big = false
+</script>
+
+<!-- <h1 class="bg-indigo-100">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+
+
+  
+  <main>
+    <!-- Navbar -> WORK IN PROGRESS -->
+    <!-- <Navbar /> -->
+  
+    <ContactCard>
+      <span slot="name"> M. Sion </span>
+  
+      <span slot="address">
+        ICI<br />
+        Pas la bas
+      </span>
+    </ContactCard>
+    <button on:click={() => (showModal = true)}> show modal </button>
+  
+    {#if showModal}
+      <Modal on:close={() => (showModal = false)}>
+        <h2 slot="header">Coucou</h2>
+        <p>Je suis le text</p>
+      </Modal>
+    {/if}
+  
+    <Button buttonType="primary">Primary</Button>
+    <Button buttonType="secondary">Secondary</Button>
+    <Button buttonType="primary" inverse={true}>Inverse Primary</Button>
+    <Button buttonType="secondary" inverse={true}>Inverse Secondary</Button>
+    <Button flat={true}>Flat</Button>
+    <Button disabled={true}>Disabled</Button>
+  
+    <Tabs {tabItems} {activeItem} on:tabChange={triggerTabChange} />
+    {#if activeItem === 'Home'}
+      <p>Welcome Home</p>
+    {:else if activeItem === 'Not Home'}
+      <p>Welcome Not Home</p>
+    {:else}
+      <p>Where are you ?</p>
+    {/if}
+  
+    <List {schools} {toStrong} />
+  
+    <div class="card">
+      <Timer />
+    </div>
+
+    <section class='flex flex-col items-center my-10'>
+        <Carousel {images}/>
+        <slot />
+    </section>
+    
+  </main>
+  
