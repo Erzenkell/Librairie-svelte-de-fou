24921477341b25a1e4d87050d8e5824f7c54d63b
Martin SION <47590798+domino659@users.noreply.github.com>
2022-09-08 13:57:34 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,46 @@
 <script>
-    export const prerender = true
-    
-    let images = [
-        {title: 'a', src: '../bg1.jpg'},
-        {title: 'b', src: '../bg2.jpg'},
-        {title: 'c', src: '../bg3.jpg'},
-        {title: 'd', src: '../bg4.jpg'},
-        {title: 'e', src: '../bg5.jpg'},
-    ]
-    import Carousel from "../lib/Carousel.svelte"
-    import Todo from "../lib/Todo.svelte"
-    import Tabs from '../lib/Tab.svelte'
-    import Timer from '../lib/Timer.svelte'
-    import List from '../lib/List.svelte'
-    import Button from '../lib/Button.svelte'
-    import ContactCard from '../lib/ContactCard.svelte'
-    import Modal from '../lib/Modal.svelte'
-    import Navbar from '../lib/Navbar.svelte'
-    import Divider from '../lib/Divider.svelte'
-    import Dropdown from "../lib/Dropdown.svelte"
-  
-    let schools = ['HETIC', 'EEMI', 'Epitech', 'Etna']
-    let toStrong = 'HETIC'
-  
-    let tabItems = ['Home', 'Not Home', '???']
-    let activeItem = 'Home'
-  
-    const triggerTabChange = (event) => {
-      activeItem = event.detail
-    }
-  
-    let showModal = false
-    let big = false
+  export const prerender = true
+
+  let images = [
+    { title: 'a', src: '../bg1.jpg' },
+    { title: 'b', src: '../bg2.jpg' },
+    { title: 'c', src: '../bg3.jpg' },
+    { title: 'd', src: '../bg4.jpg' },
+    { title: 'e', src: '../bg5.jpg' }
+  ]
+  import Carousel from '../lib/Carousel.svelte'
+  import Todo from '../lib/Todo.svelte'
+  import Tabs from '../lib/Tab.svelte'
+  import Timer from '../lib/Timer.svelte'
+  import List from '../lib/List.svelte'
+  import Button from '../lib/Button.svelte'
+  import ContactCard from '../lib/ContactCard.svelte'
+  import Modal from '../lib/Modal.svelte'
+  import Navbar from '../lib/Navbar.svelte'
+  import Divider from '../lib/Divider.svelte'
+  import Dropdown from '../lib/Dropdown.svelte'
+
+  let schools = ['HETIC', 'EEMI', 'Epitech', 'Etna']
+  let toStrong = 'HETIC'
+
+  let tabItems = ['Home', 'Not Home', '???']
+  let activeItem = 'Home'
+
+  const triggerTabChange = (event) => {
+    activeItem = event.detail
+  }
+
+  let showModal = false
+
+  let pseudo = 'MyPseudo'
+  let menuItems = ['Home', 'About', 'Contact']
+  let burgerItems = ['Home', 'About', 'Contact', 'Blog', 'Careers', 'Support']
 </script>
 
-  
 <main>
-  <!-- Navbar -> WORK IN PROGRESS -->
-  <!-- <Navbar /> -->
-  <Divider >      
-    <div slot="dividerContent">
-      Contact Card
-    </div>
+  <Divider>
+    <div slot="dividerContent">Contact Card</div>
   </Divider>
-
   <ContactCard>
     <span slot="name"> M. Sion </span>
 
@@ -53,10 +50,8 @@
     </span>
   </ContactCard>
 
-  <Divider >      
-    <div slot="dividerContent">
-      Modal
-    </div>
+  <Divider>
+    <div slot="dividerContent">Modal</div>
   </Divider>
 
   <button on:click={() => (showModal = true)}> show modal </button>
@@ -68,10 +63,8 @@
     </Modal>
   {/if}
 
-  <Divider >      
-    <div slot="dividerContent">
-      chépa
-    </div>
+  <Divider>
+    <div slot="dividerContent">Boutons</div>
   </Divider>
 
   <Button buttonType="primary">Primary</Button>
@@ -81,10 +74,8 @@
   <Button flat={true}>Flat</Button>
   <Button disabled={true}>Disabled</Button>
 
-  <Divider >      
-    <div slot="dividerContent">
-      Tabs
-    </div>
+  <Divider>
+    <div slot="dividerContent">Tabs</div>
   </Divider>
 
   <Tabs {tabItems} {activeItem} on:tabChange={triggerTabChange} />
@@ -96,59 +87,58 @@
     <p>Where are you ?</p>
   {/if}
 
-  <Divider >      
-    <div slot="dividerContent">
-      List
-    </div>
+  <Divider>
+    <div slot="dividerContent">List</div>
   </Divider>
 
   <List {schools} {toStrong} />
 
-  <Divider >      
-    <div slot="dividerContent">
-      Timer
-    </div>
+  <Divider>
+    <div slot="dividerContent">Timer</div>
   </Divider>
 
   <div class="card">
     <Timer />
   </div>
 
-  <Divider >      
-    <div slot="dividerContent">
-      Dropdown
-    </div>
+  <Divider>
+    <div slot="dividerContent">Dropdown</div>
   </Divider>
 
   <Dropdown />
 
-  <Divider >      
-    <div slot="dividerContent">
-      Carousel de fou
-    </div>
+  <Divider>
+    <div slot="dividerContent">Carousel de fou</div>
   </Divider>
 
-    <section class='flex flex-col items-center my-10'>
-      <br>
-      <p>Carousel simple fait avec Svelte & Tailwind, fonctionnalité 'magnétique', peut comprendre n'importe quel nombre de photos.</p>
-      <br> 
-      <Carousel {images}/>
-        <slot />
-    </section>
+  <section class="flex flex-col items-center my-10">
+    <br />
+    <p>
+      Carousel simple fait avec Svelte & Tailwind, fonctionnalité 'magnétique',
+      peut comprendre n'importe quel nombre de photos.
+    </p>
+    <br />
+    <Carousel {images} />
+    <slot />
+  </section>
 
-    <Divider >      
-      <div slot="dividerContent">
-        Todo list de fou
-      </div>
-    </Divider>
+  <Divider>
+    <div slot="dividerContent">Todo list de fou</div>
+  </Divider>
 
-    <br>
-    <p>Todo list stylisée avec Tailwind,ajout et suppression d'éléments, checkbox de complétion, pas d'utilisation de localstorage.</p>
-    <br> 
+  <br />
+  <p>
+    Todo list stylisée avec Tailwind,ajout et suppression d'éléments, checkbox
+    de complétion, pas d'utilisation de localstorage.
+  </p>
+  <br />
 
-    <section class="border-solid border-2 border-white-500">
+  <section class="border-solid border-2 border-white-500">
+    <Todo />
+  </section>
 
-      <Todo />
-    </section>    
+  <Divider>
+    <div slot="dividerContent">Navbar</div>
+  </Divider>
+  <Navbar {pseudo} {menuItems} {burgerItems} />
 </main>
-  
